ea5395ad6d343620cabd5aa35c2580de1a1af5c7
Tilak Patel <ptilak142@gmail.com>
2020-12-22 16:26:43 -0500

    write communicator

---
+++ newegg/communicator.py
@@ -0,0 +1,99 @@
+import requests
+from base64 import standard_b64encode
+import json
+from typing import Optional
+
+class NeweggCommunicator:
+    def __init__(self, cookies) -> None:
+        self.cookies = cookies
+
+    def add_to_cart(self, pid: str, isCombo: bool) -> bool:
+        url = 'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList='
+        if isCombo:
+            url += 'Combo.'
+        url += pid
+        try:
+            req = requests.get(url, cookies=self.cookies, timeout=2)
+            return pid in req.url
+        except:
+            return False
+
+    @staticmethod
+    def get_default_headers(newEntries: dict) -> dict:
+        defaults = {
+            'authority': 'secure.newegg.com',
+            'accept': 'application/json, text/plain, */*',
+            'x-requested-with': 'XMLHttpRequest',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.57',
+            'content-type': 'application/json',
+            'origin': 'https://secure.newegg.com',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'accept-language': 'en-US,en;q=0.9'
+            }
+        defaults.update(newEntries)
+        return defaults
+
+    def gen_session_id(self, pid: str, sid: str, isCombo: bool) -> Optional[str]:
+        def gen_data_string() -> dict:
+            data = {'SaleType':1,'ItemGroup':1,'ItemNumber': sid ,'OptionalInfos':[]}
+            if isCombo:
+                data['ItemGroup'] = 3
+                data['ItemNumber'] = pid
+            encryptedBytes = standard_b64encode(str.encode(json.dumps(data))).decode()
+            return {'ItemList': [{'ItemNumber': sid, 'ItemKey': encryptedBytes, 'Quantity': 1}], 'Actions': []}
+
+        url = 'https://secure.newegg.com/shop/api/CheckoutApi'
+        headers = self.get_default_headers({'referer': 'https://secure.newegg.com/shop/cart'})
+        data = gen_data_string()
+        try:
+            req = requests.post(url, headers=headers, data=data, cookies=self.cookies, timeout=2)
+            if req.status_code != 200:
+                return None
+            json_res = req.json()
+            return json_res['SessionID']
+        except:
+            return None
+
+    def get_transaction_number(self, session_id) -> Optional[int]:
+        url = 'https://secure.newegg.com/shop/api/InitOrderReviewApi'
+        headers = self.get_default_headers({'x-sessionid': session_id, 'referer': 'https://secure.newegg.com/shop/checkout?sessionId=' + session_id})
+        data = {'SessionID':session_id,'Actions':[{'ActionType':'AlterPanelStatus','JsonContent':json.dumps({'ActionType':'AlterPanelStatus','PanelStatus':{'ShippingAddress':'Done','DeliveryMethod':'Done','TaxID':'Done','Payment':'Todo'}})}],'EnableAsyncToken':True}
+        try:
+            req = requests.post(url, headers=headers, data=data, cookies=self.cookies, timeout=2)
+            if req.status_code != 200:
+                return None
+            return req.json()['PaymentOption']['CreditCardList'][0]['TransactionNumber']
+        except:
+            return None
+
+    def submit_card_info(self, transaction_number: int, cvv: str, session_id: str) -> bool:
+        url = 'https://secure.newegg.com/shop/api/InitOrderReviewApi'
+        headers = self.get_default_headers({'x-sessionid': session_id, 'referer': 'https://secure.newegg.com/shop/checkout?sessionId=' + session_id})
+        data = {'SessionID':session_id,'Actions':[{'ActionType':'ConfirmPayment','JsonContent':json.dumps({'ActionType':'ConfirmPayment','Cvv2': cvv, 'TransactionNumber': transaction_number, 'PaytermsCode': 'Discover'})}],'EnableAsyncToken':True}
+        try:
+            req = requests.post(url, headers=headers, data=data, cookies=self.cookies, timeout=2)
+            return req.status_code == 200
+        except:
+            return False
+
+    def validate_address(self, transaction_number: int, session_id: str, name: str, phone: str, country: str, country_long: str, state: str, city: str, address: str, zip_code: str, phone_number: str) -> bool:
+        url = 'https://secure.newegg.com/shop/api/ValidateAddress'
+        headers = self.get_default_headers({'x-sessionid': session_id, 'referer': 'https://secure.newegg.com/shop/checkout?sessionId=' + session_id})
+        data = {'TransNumber': transaction_number, 'AddressLabel': 'Untitled', 'ContactWith': name, 'Phone': phone, 'Fax': '', 'Country': country, 'State': state, 'City': city, 'Address1': address, 'Address2': '', 'ZipCode': zip_code, 'IsDefault': False, 'DisplayLines': [address, '{city}, {state} {zip_code}', country_long, phone_number], 'AddressVerifyMark': 'Verified', 'Email': None, 'DisableEmail': False, 'CompanyName': '', 'LanguageCode': None, 'IsSelected': False, 'SaveAddress': False, 'QASDisplayLines': [address, '{city}, {state} {zip_code}', country_long]}
+        try:
+            req = requests.post(url, headers=headers, data=data, cookies=self.cookies, timeout=2)
+            return req.status_code == 200
+        except:
+            return False
+
+    def submit_order(self, session_id: str) -> bool:
+        url = 'https://secure.newegg.com/shop/api/PlaceOrderApi'
+        headers = self.get_default_headers({'x-sessionid': session_id, 'referer': 'https://secure.newegg.com/shop/checkout?sessionId=' + session_id})
+        data = {'SessionID': session_id, 'IsAcceptNSCCAuth': False, 'SubscribeNewsletterFlag': False, 'CreactAccount': False, 'Password': '', 'MobileSubscribe': {}, 'LanguageCode': 'en-us', 'Cvv2': ''}
+        try:
+            req = requests.post(url, headers=headers, data=data, cookies=self.cookies, timeout=2)
+            return req.status_code == 200
+        except:
+            return False
