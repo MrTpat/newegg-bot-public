95bcc31d8007ef54b70ea92cd5a4d22d746ce5c6
Tilak Patel <ptilak142@gmail.com>
2020-12-24 04:50:33 -0500

    Add application logging

--- newegg/logger.py
+++ newegg/logger.py
@@ -12,20 +12,51 @@ class Logger:
     
     def log_err(self, s: str) -> None:
         s = f'{self.pre_text}{s}'
+        DebugLogger.log(s)
         click.secho(s, fg='red', bold=True)
 
     def log_info(self, s: str) -> None:
         s = f'{self.pre_text}{s}'
+        DebugLogger.log(s)
         click.secho(s, fg='blue')
 
     def log_success(self, s: str) -> None:
         s = f'{self.pre_text}{s}'
+        DebugLogger.log(s)
         click.secho(s, fg='green', bold=True)
 
     def log_important(self, s:str) -> None:
         s = f'{self.pre_text}{s}'
+        DebugLogger.log(s)
         click.secho(s, fg='yellow', bold=True)
 
     def handle_err(self, e: Exception) -> None:
         self.log_err(str(e))
+        DebugLogger.log_err(e)
         exit(1)
+
+class DebugLogger:
+    lines: list = []
+
+    @staticmethod
+    def log(s: str) -> None:
+        DebugLogger.lines.append(s)
+
+    @staticmethod
+    def log_err(e: Exception) -> None:
+        DebugLogger.log(str(e))
+
+    @staticmethod
+    def save_log_to_file() -> None:
+        from datetime import datetime
+        try:
+            now = datetime.now()
+            str_date = now.strftime("%m_%d_%Y_%H_%M_%S")
+            f = open(f'logs/{str_date}', 'w')
+            DebugLogger.lines.append('') #prevents no newline at EOF
+            string_to_write: str = '\n'.join(DebugLogger.lines)
+            f.write(string_to_write)
+            f.close()
+        except Exception as e:
+            Logger().handle_err(e)
+
