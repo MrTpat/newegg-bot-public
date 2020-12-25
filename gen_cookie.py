efa6b4d4d36f31ab71e0aaf3535c547ab3a99e1c
Tilak Patel <ptilak142@gmail.com>
2020-12-25 01:49:40 -0500

    Colorize gen_cookie

--- gen_cookie.py
+++ gen_cookie.py
@@ -3,19 +3,26 @@ from selenium.webdriver.chrome.options import Options
 import json
 from chromedriver_py import binary_path
 import os
+from newegg import logger
+
+
+app_logger = logger.Logger()
 
 
 def saveCookies(sel, file_name: str):
-    if not os.path.exists("./cookies"):
-        os.makedirs("./cookies")
-    d = {}
-    for c in sel.get_cookies():
-        d[c["name"]] = c["value"]
-    j = json.dumps(d)
-    f = open(f"cookies/{file_name}", "w")
-    f.write(j)
-    f.close()
-    print("saved cookies!")
+    try:
+        if not os.path.exists("./cookies"):
+            os.makedirs("./cookies")
+        d = {}
+        for c in sel.get_cookies():
+            d[c["name"]] = c["value"]
+        j = json.dumps(d)
+        f = open(f"cookies/{file_name}", "w")
+        f.write(j)
+        f.close()
+        app_logger.log_success(f"Saved cookies in {file_name}!")
+    except Exception as e:
+        app_logger.handle_err(e)
 
 
 def initBrowser(url):
@@ -29,7 +36,9 @@ def initBrowser(url):
 
 
 browser = initBrowser("https://www.newegg.com")
-file_name = input("Enter cookie file name to save to cookies folder: ")
-input("Press key when page is done loading after you logged in: ")
+app_logger.log_important("Enter cookie file name to save to cookies folder:")
+file_name = input()
+app_logger.log_important("Press key when page is done loading after you logged in:")
+input()
 saveCookies(browser, file_name)
 browser.close()
